3f0343bda219b7b78a2cca91c782661599bbe30c
spugnoid <spugnoid@gmail.com>
2019-09-02 11:00:39 -0700

    Change OBBLM to NAFLM

--- team_webservice.php
+++ team_webservice.php
@@ -2,10 +2,10 @@
 require("header.php");
 
 if (!Coach::isLoggedIn())
-    die("You must be logged into OBBLM to use this webservice.");
+    die("You must be logged into NAFLM to use this webservice.");
 
 $teamId = $_REQUEST["teamId"];
 $team = new Team_HTMLOUT($teamId);
 $team->handleActions($team->allowEdit());
-    
+
 ?>
